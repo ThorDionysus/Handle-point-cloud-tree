c314c981478924bc28aa2111b0095f281859dbe1
ThorDionysus <139136882+ThorDionysus@users.noreply.github.com>
2023-11-03 22:33:07 +0800

    Update k-means.py

--- k-means.py
+++ k-means.py
@@ -2,8 +2,8 @@ import open3d as o3d
 import numpy as np
 from sklearn.cluster import KMeans
 
-# import point cloud data
-point_cloud = o3d.io.read_point_cloud(r"C:\cloudsegmentation\S-F.ply")
+# import point cloud data, remember to alter the "flie_path"to the point cloud you want to handle
+point_cloud = o3d.io.read_point_cloud(r"file_path")
 colors = np.asarray(point_cloud.colors)
 points = np.asarray(point_cloud.points)
 
@@ -18,7 +18,7 @@ for cluster_id in range(max(cluster_labels) + 1):
     cluster_center = np.mean(cluster_colors, axis=0)
     cluster_centers.append(cluster_center)
 
-sorted_cluster_indices = np.argsort(cluster_centers, axis=0)[:, 0]  # 根据R通道的值排序
+sorted_cluster_indices = np.argsort(cluster_centers, axis=0)[:, 0] 
 
 sorted_point_clouds = [o3d.geometry.PointCloud() for _ in range(len(cluster_centers))]
 
@@ -32,5 +32,6 @@ for sorted_cluster_id, original_cluster_id in enumerate(sorted_cluster_indices):
     sorted_point_cloud.points = o3d.utility.Vector3dVector(sorted_cluster_points)
     sorted_point_clouds[sorted_cluster_id] = sorted_point_cloud
 
+# Remenber alter the "flie_path" to the path you want to save the result
 for sorted_cluster_id, sorted_cluster_point_cloud in enumerate(sorted_point_clouds):
-    o3d.io.write_point_cloud(f"C:\cloudsegmentation\S-KMeans-Sorted{sorted_cluster_id}.ply", sorted_cluster_point_cloud)
+    o3d.io.write_point_cloud(f"flie_path\KMeans-Sorted{sorted_cluster_id}.ply", sorted_cluster_point_cloud)
